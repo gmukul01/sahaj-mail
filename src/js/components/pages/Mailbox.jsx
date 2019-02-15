d591638e36bbacba8543a83306a1e0ba0e518680
gmukul01 <mukulbansal01@gmail.com>
2019-02-16 01:57:55 +0700

    Created basic mailbox layout scss

--- src/js/components/pages/Mailbox.jsx
+++ src/js/components/pages/Mailbox.jsx
@@ -1,5 +1,13 @@
 import React from 'react';
 
-const Mailbox = ({ userName }) => <p>{userName}</p>;
+const Mailbox = ({ userName }) => {
+	return (
+		<div className="mailbox">
+			<div className="sidebar">sidebar</div>
+			<div className="header">header</div>
+			<div className="content">content</div>
+		</div>
+	);
+};
 
 export default React.memo(Mailbox);
