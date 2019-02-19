cfd435196ebfe5aac9f8c6cea25e4419741d9da9
gmukul01 <mukulbansal01@gmail.com>
2019-02-20 00:03:31 +0700

    Fixed issue of the read email popup showing same email

--- src/js/components/email/EmailList.jsx
+++ src/js/components/email/EmailList.jsx
@@ -1,25 +1,12 @@
-import React, { useState, Fragment } from 'react';
+import React from 'react';
 
 import formatTime from 'util/formatTime';
 import EmailListItem from './EmailListItem';
-import ReadEmail from './ReadMail';
 
 const EmailList = ({ onRead, emails, selectedEmails, addToSelectedEmails }) => {
-	const [showModal, setModalState] = useState(false),
-		onCloseModal = () => setModalState(false),
-		onOpenModal = id => {
-			onRead([id]);
-			setModalState(true);
-		},
-		emailList = emails.map(({ timestamp, ...rest }) => {
-			const onEmailClick = () => onOpenModal(rest.id);
-			return (
-				<Fragment key={rest.id}>
-					<ReadEmail {...{ emailDetails: rest, showModal, onCloseModal }} />
-					<EmailListItem {...{ ...rest, selectedEmails, addToSelectedEmails, onEmailClick, timestamp: formatTime(timestamp) }} />
-				</Fragment>
-			);
-		});
+	const emailList = emails.map(({ timestamp, ...rest }) => {
+		return <EmailListItem key={rest.id} {...{ ...rest, selectedEmails, addToSelectedEmails, onRead, timestamp: formatTime(timestamp) }} />;
+	});
 
 	return <div className="email-list">{emailList}</div>;
 };
--- src/js/components/email/EmailListItem.jsx
+++ src/js/components/email/EmailListItem.jsx
@@ -1,24 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import classNames from 'classnames';
+
 import CheckBox from 'components/CheckBox';
+import ReadEmail from './ReadMail';
 
 const EmailListItem = props => {
-	const { selectedEmails, addToSelectedEmails, id, onEmailClick, from, subject, isRead, category, hasAttachment, timestamp } = props;
-	const listClassName = classNames('email-list-item', { selected: selectedEmails.includes(id + '') || isRead });
+	const [showModal, setModalState] = useState(false),
+		onCloseModal = () => setModalState(false),
+		onEmailClick = () => {
+			props.onRead([props.id]);
+			setModalState(true);
+		},
+		{ selectedEmails, addToSelectedEmails, id, from, subject, body, isRead, category, hasAttachment, timestamp } = props,
+		listClassName = classNames('email-list-item', { selected: selectedEmails.includes(id + '') || isRead });
 
 	return (
-		<div className={listClassName}>
-			<div className="email-list-item__checkbox">
-				<CheckBox id={id} selected={selectedEmails.includes(id)} onChange={addToSelectedEmails} />
+		<>
+			<ReadEmail {...{ emailDetails: { from, subject, body }, showModal, onCloseModal }} />
+			<div className={listClassName}>
+				<div className="email-list-item__checkbox">
+					<CheckBox id={id} selected={selectedEmails.includes(id)} onChange={addToSelectedEmails} />
+				</div>
+				<div onClick={onEmailClick} className="email-list-item__rest">
+					<p className="email-list-item__from">{from.name}</p>
+					<div className="email-list-item__category">{category && <button className={`chips-btn ${category.toLowerCase()}`}>{category}</button>}</div>
+					<p className="email-list-item__subject">{subject || 'no subject'}</p>
+					<p className="email-list-item__attachment"> {hasAttachment ? <i className="fas fa-paperclip" /> : null}</p>
+					<p className="email-list-item__timestamp">{timestamp}</p>
+				</div>
 			</div>
-			<div onClick={onEmailClick} className="email-list-item__rest">
-				<p className="email-list-item__from">{from.name}</p>
-				<div className="email-list-item__category">{category && <button className={`chips-btn ${category.toLowerCase()}`}>{category}</button>}</div>
-				<p className="email-list-item__subject">{subject || 'no subject'}</p>
-				<p className="email-list-item__attachment"> {hasAttachment ? <i className="fas fa-paperclip" /> : null}</p>
-				<p className="email-list-item__timestamp">{timestamp}</p>
-			</div>
-		</div>
+		</>
 	);
 };
 
