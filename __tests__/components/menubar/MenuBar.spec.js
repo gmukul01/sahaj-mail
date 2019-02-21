08d3785778284517bef0057921323640ee2e6fd3
gmukul01 <mukulbansal01@gmail.com>
2019-02-22 00:33:08 +0700

    Added tests for MenuBar

---
+++ __tests__/components/menubar/MenuBar.spec.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import renderer from 'react-test-renderer';
+
+import MenuBar from 'components/menubar/MenuBar';
+
+describe('MenuBar Component', () => {
+	const mockSendEmail = jest.fn(),
+		initialProps = {
+			totalUnread: 2,
+			sendEmail: mockSendEmail
+		};
+
+	it('should match snapshot', () => {
+		const menubar = renderer.create(<MenuBar {...initialProps} />).toJSON();
+		expect(menubar).toMatchSnapshot();
+	});
+
+	it('should call mockSendEmail when sendEmail is called', () => {
+		const menubar = shallow(<MenuBar {...initialProps} />);
+		menubar.find('ComposeEmail').prop('sendEmail')('Dummy');
+		expect(mockSendEmail.mock.calls[0][0]).toBe('Dummy');
+	});
+});
